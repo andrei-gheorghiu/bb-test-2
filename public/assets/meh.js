8f8cd5b7ea4622253a3f26844a53bd26fa7c9ddf
Andrei Gheorghiu <andrei.gheorghiu@kmap.io>
2018-10-20 20:41:56 +0100

    cleanup

--- public/assets/meh.js
+++ public/assets/meh.js
@@ -1,24 +1,29 @@
 'use strict';
 
-var _loadJsonP = function _loadJsonP(url) {
-			var ref = window.document.getElementsByTagName('script')[0],
-					script = window.document.createElement('script');
-			script.src = url + (url.indexOf('?') + 1 ? '&' : '?');
-			ref.parentNode.insertBefore(script, ref);
-			script.onload = function () {
-				this.remove();
-			};
-		},
-		_callFlickr = function _callFlickr(animal) {
-			_loadJsonP('' + ('https://api.flickr.com/services/feeds/photos_public.gne?' + '&tags=') + animal + '&format=json');
-		},
-		_getAnimal = function _getAnimal(event) {
-			var anchor = event.target.closest('.animal');
-			if (anchor) {
-				_callFlickr(anchor.innerHTML);
-			}
-		},
-		_src = void 0;
+var _src = void 0;
+document.querySelector('nav').addEventListener('click', _getAnimal);
+
+function _loadJsonP(url) {
+	var ref = window.document.getElementsByTagName('script')[0],
+			script = window.document.createElement('script');
+	script.src = url + (url.indexOf('?') + 1 ? '&' : '?');
+	ref.parentNode.insertBefore(script, ref);
+	script.onload = function () {
+		this.remove();
+	};
+}
+
+function _callFlickr(animal) {
+	_loadJsonP('' + ('https://api.flickr.com/services/feeds/photos_public.gne?' + '&tags=') + animal + '&format=json');
+}
+
+function _getAnimal(event) {
+	var anchor = event.target.closest('.animal');
+	if (anchor) {
+		_callFlickr(anchor.innerHTML);
+	}
+}
+
 function jsonFlickrFeed(data) {
 	if (data && data.items && data.items.length) {
 		document.querySelector('#images').innerHTML = '';
@@ -31,4 +36,3 @@ function jsonFlickrFeed(data) {
 		}
 	}
 }
-document.querySelector('nav').addEventListener('click', _getAnimal);
